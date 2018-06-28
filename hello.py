9ea6a9e3469b9d112a4fab5f41461defbf892182
A M <chidambaram.a_m.ext@nokia.com>
2018-06-28 21:46:00 +0530

    added hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+ Volume in drive C is System
+ Volume Serial Number is FCAB-D61B
+
+ Directory of C:\Users\chidamam\Desktop\my_prj
+
+28-06-2018  21:42    <DIR>          .
+28-06-2018  21:42    <DIR>          ..
+28-06-2018  21:42                 0 hello.py
+               1 File(s)              0 bytes
+               2 Dir(s)  420,963,131,392 bytes free
